e39e6cf2de9d9511bb79bfbdf34a673ff7c0231d
benjimor <benji_moreau@hotmail.fr>
2019-02-27 16:54:09 +0100

    update to version 2.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pycali',
-                 version='2.0.0',
+                 version='2.0.1',
                  description='A python package that defines a partial order over RDF licenses',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
