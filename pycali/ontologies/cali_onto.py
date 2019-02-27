0d87eb2c763fcda786ea93b403f826c02c72ebb5
benjimor <benji_moreau@hotmail.fr>
2019-02-27 15:58:00 +0100

    Refactoring ontologies

---
+++ pycali/ontologies/cali_onto.py
@@ -0,0 +1,22 @@
+from rdflib import Namespace
+
+from pycali.vocabulary import ODRL
+
+# Namespace
+CALI = Namespace('http://cali.priloo.univ-nantes.fr/ontology#')
+ODRS = Namespace('http://schema.theodi.org/odrs#')
+
+# Classes
+DeonticState = CALI['DeonticState']
+
+# Properties
+lessRestrictiveThan = CALI['lessRestrictiveThan']
+compatibleWith = ODRS['compatibleWith']
+
+# Resources
+# Deontic states
+Undefined = CALI['Undefined']
+Recommended = CALI['Recommended']
+Permission = ODRL['Permission']
+Prohibition = ODRL['Prohibition']
+Duty = ODRL['Duty']
